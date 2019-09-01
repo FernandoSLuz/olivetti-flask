cf09b93ae90e39b2557bc8217a6e043673345032
FernandoSLuz <fernandosilvadaluz@gmail.com>
2019-09-01 11:13:28 -0300

    50

--- webhook.py
+++ webhook.py
@@ -77,7 +77,7 @@ def returnMessage(tempUserStep, recievedMessage):
         if bd.searchByUsername(tempUserStep.nome_funcionario):
             tempUserStep.loja = bd.SelectLojas_Unique(int(recievedMessage))
             updateUserStep(tempUserStep)
-            sendMessage(tempUserStep, "Muito bem, "+tempUserStep.nome_funcionario+"! agora que você está cadastrado(a), trarei as novidades do shopping Parque D. Pedro para você! \\nDigite a opção desejada:\\n1- Descontos\\n2- Avisos e Informações\\n3- Receber avisos automáticos\\n4- Desabilitar avisos automáticos")
+            sendMessage(tempUserStep, "Caralho "+tempUserStep.nome_funcionario+"! agora que você está cadastrado(a), trarei as novidades do shopping Parque D. Pedro para você! \\nDigite a opção desejada:\\n1- Descontos\\n2- Avisos e Informações\\n3- Receber avisos automáticos\\n4- Desabilitar avisos automáticos")
             tempUserStep.passo = "A2"
         else:
             sendMessage(tempUserStep, "Infelizmente não localizei seu cadastro no Portal, mas já enviei uma solicitação ao seu líder e, assim que o cadastro for completado, você irá receber descontos e novidades aqui pelo Pepe!")
