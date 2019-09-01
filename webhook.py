63d0f55609fdeaadc267641fcb0b5e07e6732ba2
FernandoSLuz <fernandosilvadaluz@gmail.com>
2019-09-01 14:53:14 -0300

    50

--- webhook.py
+++ webhook.py
@@ -1,4 +1,3 @@
-# -*- encoding: utf-8 -*-
 import sys
 import os
 import time
@@ -44,6 +43,7 @@ def sendMessageToLeader(leader, requester):
     url = "https://api.wassenger.com/v1/messages"
 
     messageBody = "Olá " + str(leader[1]) + "! um funcionário solicitou acesso à plataforma de serviços IntranetMall:\\n\\nNome: " + requester.nome_funcionario + "\\nTelefone: " + requester.telefone 
+    messageBody = unicode(messageBody,'utf-8')
     payload = "{\"phone\":\""+str(leader[0])+"\",\"priority\":\"urgent\",\"message\":\""+messageBody+"\"}"
     headers = {
         'content-type': "application/json",
@@ -58,7 +58,7 @@ def sendMessage(userTosendMessage, messageBody):
     url = "https://api.wassenger.com/v1/messages"
 
 
-    payload = "{\"phone\":\""+userTosendMessage.telefone+"\",\"priority\":\"urgent\",\"message\":\""+messageBody+"\"}"
+    payload = "{\"phone\":\""+userTosendMessage.telefone+"\",\"priority\":\"urgent\",\"message\":\""+unicode(messageBody,'utf-8')+"\"}"
     headers = {
         'content-type': "application/json",
         'token': "905bd94b9d3a26df733849887c838b9cc5ee1538b72fb1937edf027d5b7b71c71b2c54f1c894e4a2"
