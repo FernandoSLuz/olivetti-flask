935163925715026199b566a0e803bd62306fb065
FernandoSLuz <fernandosilvadaluz@gmail.com>
2019-09-01 14:58:33 -0300

    50

--- webhook.py
+++ webhook.py
@@ -57,7 +57,7 @@ def sendMessage(userTosendMessage, messageBody):
     #print(userTosendMessage.telefone + " -- " + messageBody)
     url = "https://api.wassenger.com/v1/messages"
 
-    encodedMessage = str(messageBody.encode('utf-8'))
+    encodedMessage = str(messageBody.encode())
     payload = "{\"phone\":\""+userTosendMessage.telefone+"\",\"priority\":\"urgent\",\"message\":\""+encodedMessage+"\"}"
     headers = {
         'content-type': "application/json",
