2b83899194510c61ce8015415dad5d80b470300a
FernandoSLuz <fernandosilvadaluz@gmail.com>
2019-09-01 03:28:34 -0300

    dbs

--- webhook.py
+++ webhook.py
@@ -9,12 +9,35 @@ from flask import request
 
 blueprint = flask.Blueprint('webhook', __name__)
 
-@blueprint.route('/webhook', methods=[ 'POST' ])
+class UserStep():
+    telefone = ""
+    setor = ""
+    loja = ""
+    passo = ""
+
+UserSteps = [] 
+
+
+@blueprint.route('/webhook', methods=[ 'POST', 'GET' ])
 def webhook():
     form = request.get_json(silent=True, force=True)
-    res = (json.dumps(form, indent=3))
-    print(res)
+    #res = (json.dumps(form, indent=3))
+    #print(res)
+    recievedPhoneStr = form['data']['fromNumber']
+    phoneStr = ""
+    for item in UserSteps:
+        if item.telefone == str(recievedPhoneStr):
+            phoneStr = str(recievedPhoneStr)
+    if phoneStr == "":
+        print('not found, adding number ' + str(recievedPhoneStr))
+        phoneStr = str(recievedPhoneStr)
+        tempUserStep = UserStep
+        tempUserStep.telefone = phoneStr
+        UserSteps.append(tempUserStep)
+    else:
+        print('number exists! ' + phoneStr)
+
     context = {
-        'title':'webhook | Update'
+        'title':'webhook | ' + phoneStr
     }
     return context
