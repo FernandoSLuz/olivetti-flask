df158dd83f634104da488c54d2c2d654d43fd4d3
FernandoSLuz <fernandosilvadaluz@gmail.com>
2019-09-01 01:46:19 -0300

    dbs

--- app.py
+++ app.py
@@ -31,4 +31,4 @@ if __name__ == "__main__":
     root_module = os.path.abspath(os.path.curdir)
     sys.path.append(root_module)
 
-    app.run(host='0.0.0.0', port=3311)
+    app.run(host='0.0.0.0')
