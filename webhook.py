cdc7f8fefd6c414f97198a3fefcc6c964d71760d
FernandoSLuz <fernandosilvadaluz@gmail.com>
2019-09-01 11:15:11 -0300

    50

--- webhook.py
+++ webhook.py
@@ -65,7 +65,7 @@ def returnMessage(tempUserStep, recievedMessage):
     elif tempUserStep.passo == 'B2':
         tempUserStep.nome_funcionario = recievedMessage
         updateUserStep(tempUserStep)
-        sendMessage(tempUserStep, "Muito bem, "+tempUserStep.nome_funcionario+". \\n Voce poderia me dizer em qual setor trabalha?" + "\\n" + bd.SelectSetores())
+        sendMessage(tempUserStep, "Caralho "+tempUserStep.nome_funcionario+"!. \\n Voce poderia me dizer em qual setor trabalha?" + "\\n" + bd.SelectSetores())
         tempUserStep.passo = "B3"
     elif tempUserStep.passo == 'B3':
         tempUserStep.setor = bd.SelectSetores_Unique(int(recievedMessage))
@@ -77,7 +77,7 @@ def returnMessage(tempUserStep, recievedMessage):
         if bd.searchByUsername(tempUserStep.nome_funcionario):
             tempUserStep.loja = bd.SelectLojas_Unique(int(recievedMessage))
             updateUserStep(tempUserStep)
-            sendMessage(tempUserStep, "Caralho "+tempUserStep.nome_funcionario+"! agora que você está cadastrado(a), trarei as novidades do shopping Parque D. Pedro para você! \\nDigite a opção desejada:\\n1- Descontos\\n2- Avisos e Informações\\n3- Receber avisos automáticos\\n4- Desabilitar avisos automáticos")
+            sendMessage(tempUserStep, "Muito bem, "+tempUserStep.nome_funcionario+"! agora que você está cadastrado(a), trarei as novidades do shopping Parque D. Pedro para você! \\nDigite a opção desejada:\\n1- Descontos\\n2- Avisos e Informações\\n3- Receber avisos automáticos\\n4- Desabilitar avisos automáticos")
             tempUserStep.passo = "A2"
         else:
             sendMessage(tempUserStep, "Infelizmente não localizei seu cadastro no Portal, mas já enviei uma solicitação ao seu líder e, assim que o cadastro for completado, você irá receber descontos e novidades aqui pelo Pepe!")
