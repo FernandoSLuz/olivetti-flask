8b1e2cf3660be1dbfa7613f7184be8361cf0da6f
FernandoSLuz <fernandosilvadaluz@gmail.com>
2019-10-05 12:38:00 -0300

    dialogflow

--- dialogflowBackend.py
+++ dialogflowBackend.py
@@ -27,7 +27,7 @@ def checkNumberStatus():
 def testintents():
     form = request.get_json(silent=True, force=True)
     res = (json.dumps(form, indent=3))
-    print("hello")
+    return("hello")
     #detect_intent_texts("chatbot-olivetti", str(form['sessionId']), str(form['message']), str(form['languageCode']))
 
 
